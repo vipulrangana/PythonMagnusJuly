e657b28029d0d6bf2e9b3bf12f9d31841af2ec94
Vipul Rangana <vipulrangana@gmail.com>
2023-07-26 23:01:19 -0700

    Day4_26_July_2023

---
+++ Day4/ListExample.py
@@ -0,0 +1,18 @@
+a1 = [10,30,23,'abc',10.2,'hello']
+print(a1)
+print(a1[1])
+print(a1[0:4])
+print(a1[3:6])
+print(a1[2:])
+print(a1[:5])
+
+a1.append(120)
+print(a1)
+a1.remove(30)
+print(a1)
+a1.insert(1,30)
+print(a1)
+a1.pop()
+print(a1)
+a1.pop(1)
+print(a1)
