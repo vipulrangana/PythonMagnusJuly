923e6a1841981450d6b5c5a398eed44a32681eff
Vipul Rangana <vipulrangana@gmail.com>
2023-08-02 12:53:46 -0700

    Day8_01_Aug_2023

--- HandsOn/Conditional/Q4.py
+++ HandsOn/Conditional/Q4.py
@@ -1,1 +1,10 @@
 a = int(input('Enter the score A: '))
+b = int(input('Enter the score B: '))
+
+if a > 300 or b > 300:
+    if (a+b) < 500:
+        print('Can Team Up')
+    else:
+        print('Cannot Team Up')
+else:
+    print('Cannot Team Up')
