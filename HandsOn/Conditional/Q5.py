1013ab6478736d0258c53185ecc73dc98d8e5b4f
Vipul Rangana <vipulrangana@gmail.com>
2023-08-02 13:02:32 -0700

    Day8_01_Aug_2023

---
+++ HandsOn/Conditional/Q5.py
@@ -0,0 +1,8 @@
+a = float(input('Enter the 1st angle: '))
+b = float(input('Enter the 2nd angle: '))
+c = float(input('Enter the 3rd angle: '))
+
+if a + b + c == 180:
+    print('This is a triangle')
+else:
+    print('This is not a triangle')
